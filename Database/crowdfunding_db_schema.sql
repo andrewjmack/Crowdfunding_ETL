86dcbaf4c082d4327087bbfac20ba5af03691878
andrewjmack <andrewmack5274@gmail.com>
2024-05-13 22:37:52 -0600

    updated notebook and readme, addition of Database folder with schema, ERD and screenshots

---
+++ Database/crowdfunding_db_schema.sql
@@ -0,0 +1,63 @@
+-- Schema for project 2: crowdfunding ETL
+
+-- Drop table code provided for convenience if table recreation is needed:
+-- DROP TABLE
+--     category,
+--     subcategory,
+--     contacts,
+--     campaign
+--     ;
+
+-- Create tables in order considering foreign keys:
+CREATE TABLE category (
+    category_id VARCHAR(10) NOT NULL,
+    category VARCHAR(255) NOT NULL,
+    PRIMARY KEY (category_id)
+    );
+
+CREATE TABLE subcategory (
+    subcategory_id VARCHAR(10) NOT NULL,
+    subcategory VARCHAR(255) NOT NULL,
+    PRIMARY KEY (subcategory_id)
+    );
+
+CREATE TABLE contacts (
+    contact_id INT NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    PRIMARY KEY (contact_id)
+    );
+
+CREATE TABLE campaign (
+    cf_id INT NOT NULL,
+    contact_id INT NOT NULL,
+    company_name VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    goal FLOAT NOT NULL,
+    pledged FLOAT NOT NULL,
+    outcome VARCHAR(50) NOT NULL,
+    backers_count INT NOT NULL,
+    country VARCHAR(10) NOT NULL,
+    currency VARCHAR(10) NOT NULL,
+    launched_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    category_id VARCHAR(10) NOT NULL,
+    subcategory_id VARCHAR(10) NOT NULL,
+    PRIMARY KEY (cf_id),
+    FOREIGN KEY (contact_id) REFERENCES contacts(contact_id),
+    FOREIGN KEY (category_id) REFERENCES category(category_id),
+    FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id)
+    );
+
+-- Table data should be loaded in this order to avoid failure due to foreign key issues:
+--     - category.csv
+--     - subcategory.csv
+--     - contacts.csv
+--     - campaign.csv
+
+-- Check data import via separate table queries:
+    -- SELECT * FROM category;
+    -- SELECT * FROM subcategory;
+    -- SELECT * FROM contacts;
+    -- SELECT * FROM campaign;
--- README.md
+++ README.md
@@ -1,10 +1,35 @@
 # Crowdfunding_ETL
-DU Data Analytics Project 2 (May 2024)
+DU Data Analytics Project 2 | May 2024
+Peter Lovberg & Andrew Mack
 
 ## Purpose
-A collaborative extract-transform-load ("ETL") mini project, which involved building an ETL pipeline using Python, Pandas, and either Python dictionary methods or regular expressions to extract and transform the data. After data transformation, four CSV files were created with the file data informing the creation of an ERD and a table schema. Finally, the CSV file data was loaded into a Postgres SQL database.
+A collaborative extract-transform-load ("ETL") mini project, which involved building an ETL pipeline with Python, Pandas and PostgresSQL. Through data transformation of Excel data in a Jupyter Notebook, four CSV files were created, which formed the basis for an ERD and table schema. Finally, the CSV file data was loaded into a Postgres SQL database (see screenshots of database tables).
+
+## Summary of Instructions
+- Create the Category and Subcategory DataFrames
+- Create the Campaign DataFrame
+- Create the Contacts DataFrame
+- Create the Crowdfunding Database
+
+## Contents
+- README.md
+- ETL_Mini_Project_PLovberg_AMack.ipynb
+- Database
+    - Crowdfunding ERD 
+    - crowdfunding_db_schema.sql
+    - Campaign Screenshot
+    - Category Screenshot
+    - Contacts Screenshot
+    - Subcategory Screenshot
+- Resources
+    - campaign.csv
+    - category.csv
+    - contacts.csv
+    - subcategory.csv
 
 ## Resources
-
-Reference for merging three dataframes:
+- Class instruction and activities
+- Starter content: initial Jupyter Notebook and .xlsx data
+- Bootcampspot instructions and hints
+- Reference for merging three dataframes:
 https://stackoverflow.com/questions/23668427/pandas-three-way-joining-multiple-dataframes-on-columns
